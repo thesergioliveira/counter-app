d801aa355c4ee3780ef6e218d83af1d89b4b8e30
sergio.oliveira@deecoob.com <sergio.oliveira@deecoob.com>
2022-07-22 18:08:26 +0200

    implemented counter with increment and decrement button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Header, Container, Message, Label } from "semantic-ui-react";
+import "./App.css";
 
-function App() {
+// TODO: Add a decrement button. Requirements: the can not be negative. If that happens setCounter back to 0 and display error message.
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [error, setError] = useState(false);
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+    if (counter <= 0) {
+      setError(true);
+      setCounter(0);
+    }
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+    setError(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" data-test="app-component">
+      <Header as="h2" data-test="counter-label">
+        This is the value of the counter:&nbsp;
+        <Label circular color="violet" size="big">
+          <span data-test="counter-display">{counter}</span>
+        </Label>
+      </Header>
+      <div className="button-container">
+        <Button
+          data-test="button-increment"
+          onClick={handleIncrement}
+          color="green"
+          content="+"
+        />
+        <Button
+          data-test="button-decrement"
+          onClick={handleDecrement}
+          color="red"
+          content="-"
+        />
+      </div>
+      {error && <Message>The counter must not go negative</Message>}
+    </Container>
   );
-}
+};
 
 export default App;
